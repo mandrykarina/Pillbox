e80c6064effb0ee341bb66fed03a7f03c8af0de0
Arina <98313159+mandrykarina@users.noreply.github.com>
2024-04-22 11:45:16 +0300

    Add files via upload

---
+++ app/app.py
@@ -0,0 +1,83 @@
+import sqlite3
+import os
+
+from flask import Flask, render_template, request, g, flash
+from yandex_music import Client
+from flask_login import LoginManager
+from FDataBase import FDataBase
+from werkzeug.security import generate_password_hash, check_password_hash
+
+DATABASE = '/templates/app.db'
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'app.db')))
+
+token = 'y0_AgAAAAA2DZGJAAG8XgAAAAD4hlk7I8DRDKyCS7Sj-76dHMBPd9IUD1Q'
+client = Client(token).init()
+# login_manager = LoginManager(app)
+
+
+def connect_db():
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+def create_db():
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
+
+
+def get_db():
+    '''соединение с бд, если его еще нет'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+
+@app.teardown_appcontext
+def close_db(error):
+    '''закрываем соединение с бд, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.route('/main', methods=["GET", "POST"])
+@app.route('/', methods=["GET", "POST"])
+def main():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        # Получаем значение названия трека из формы
+        track_name = request.form['track_name']
+        # Поиск трека по названию
+        search_results = client.search(track_name)
+
+        # Создаем список треков для передачи в шаблон HTML
+        trackss = []
+        # через цикл заполняем массив словарями (потом поможет при разработки БД)
+        for elem in search_results.tracks.results:
+            track_info = {
+                'title': elem.title,
+                'artist': elem.artists[0].name,
+                'track_id': elem.id
+            }
+            trackss.append(track_info)
+
+        return render_template('main.html', tracks=trackss)
+    else:
+        # пустой массив не отобразит никаких треков
+        return render_template('main.html', tracks=[])
+
+
+@app.route('/begin', methods=["GET", "POST"])
+def begin():
+    return render_template('begin.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
